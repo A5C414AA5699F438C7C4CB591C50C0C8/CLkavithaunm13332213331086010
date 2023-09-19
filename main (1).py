5b62c3f4c61e4728bba0a26235f408f5b42dabf5
A5C414AA5699F438C7C4CB591C50C0C8 <144371139+A5C414AA5699F438C7C4CB591C50C0C8@users.noreply.github.com>
2023-09-19 19:17:56 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,22 +1,35 @@
-#1.2 leap year
-"""
-year % 4 == 0 &
-year % 100 !== 0 /
-year % 400 == 0
+class BankAccount:
 
-"""
+  def __init__(self, account_number, account_holder_name, initial_balance=0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
+
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      return f"Deposited ${amount}. New balance: ${self.__account_balance}"
+    else:
+      return "Invalid deposit amount"
+
+  def withdraw(self, amount):
+    if 0 < amount <= self.__account_balance:
+      self.__account_balance -= amount
+      return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
+    else:
+      return "Insufficient funds or invalid withdrawal amount"
+
+  def display_balance(self):
+    return f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
 
 
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    return True
-  else:
-    return False
+# Testing the BankAccount class
+if __name__ == "__main__":
+  # Create an instance of BankAccount
+  account = BankAccount("123456789", "kavitha", 1000)
 
-
-year = int(input("Enter a year :"))
-
-if isLeapYear(year):
-  print('{} is a leap year .'.format(year))
-else:
-  print('is not a leap year.'.format(year))
+  # Test deposit and withdrawal
+  print(account.display_balance())
+  print(account.deposit(500))
+  print(account.withdraw(200))
+  print(account.display_balance())
